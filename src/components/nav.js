aa4059383b83bec5b1c29e47ec0dc7fbc5e4905e
Anusikha Jha <anusikhajha@gmail.com>
2023-12-20 21:02:33 +0530

    Fix Static Files

--- src/components/nav.js
+++ src/components/nav.js
@@ -206,7 +206,7 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/aman_resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a className="resume-button" href="/anusikha_resume.pdf" target="_blank" rel="noopener noreferrer">
       Resume
     </a>
   );
